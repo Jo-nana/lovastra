6bb475203907ebcae6ee4339342d92d5219e5ab2
Johanna Florange <florangejohanna@gmail.com>
2023-05-10 13:45:38 +0200

    added block with grid

--- src/components/routes/dashboard/dashboard.component.jsx
+++ src/components/routes/dashboard/dashboard.component.jsx
@@ -1,7 +1,16 @@
+import './dashboard.styles.scss';
+
 const Dashboard = () => {
-  <div>
-    <h1> Dashboard</h1>
-  </div>
+
+  return (
+<div className="container">
+  <div className="block block1">Block 1</div>
+  <div className="block block2">Block 2</div>
+  <div className="block block3">Block 3</div>
+  <div className="block block4">Block 4</div>
+</div>
+
+  )
 }
 
 export default Dashboard;
